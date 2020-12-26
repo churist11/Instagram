aa40780434a2ba092e411086d2b26297a031dc27
Shoutarou.Sakurai <naritake109@yahoo.ne.jp>
2020-12-26 19:48:51 +0900

    Add tab bar controller to switch 3 scenes

---
+++ Instagram/Controller/TabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  TabBarController.swift
+//  Instagram
+//
+//  Created by 櫻井将太郎 on 2020/12/26.
+//  Copyright © 2020 shoutarou.sakurai. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
