ca07b05f33b8a2519192a12903b207d9b201ca6b
Shoutarou.Sakurai <naritake109@yahoo.ne.jp>
2020-12-26 16:53:24 +0900

    Prepare screens (UIviewController file, storyboard , storyboard ID)

---
+++ Instagram/Controller/SettingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingViewController.swift
+//  Instagram
+//
+//  Created by 櫻井将太郎 on 2020/12/26.
+//  Copyright © 2020 shoutarou.sakurai. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
