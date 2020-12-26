57e78dbf20ed058dda7104b1a9a0741e8250055d
Shoutarou.Sakurai <naritake109@yahoo.ne.jp>
2020-12-27 01:31:10 +0900

    Implement new account creation

--- Instagram/Controller/LoginViewController.swift
+++ Instagram/Controller/LoginViewController.swift
@@ -18,6 +18,7 @@ final class LoginViewController: UIViewController {
 	@IBOutlet weak var mailAddressTextField: UITextField!
 	@IBOutlet weak var passwordTextField: UITextField!
 	@IBOutlet weak var displayNameTextField: UITextField!
+	@IBOutlet weak var alertLabel: UILabel!
 
 
 	// MARK: - LifeCycle
@@ -37,8 +38,72 @@ final class LoginViewController: UIViewController {
 	}
 
 	@IBAction func handleCreateAccountButton(_ sender: UIButton) {
+
+		// Optional binding
+		if let email = self.mailAddressTextField.text,
+			let pass = self.passwordTextField.text,
+			let name = self.displayNameTextField.text {
+
+			// If There is empty textfield, do notihng
+			guard (email.isEmpty == false) && (pass.isEmpty == false) && (name.isEmpty == false) else {
+				print("DEBUG_PRINT: There is empty text field.")
+				return
+			}
+
+			// Confirm All text field is inputed, then create new account
+			Auth.auth().createUser(withEmail: email, password: pass) { (result, error) in
+
+				// <<Fail creation>>
+				// 1. Catch error
+				guard error == nil else {
+
+					// 2. display alert for user
+					self.alertLabel.isHidden = false
+					self.alertLabel.text = "\(error!.localizedDescription)"
+
+					print("DEBUG_PRINT: \(error!)")
+
+					// 3. Do nothing
+					return
+				}
+
+
+				// <<Success creation>>
+				print("DEBUG_PRINT: Success account creation!!")
+
+				// 1. Re-hide alert label
+				self.alertLabel.isHidden = true
+
+				// 2. Setting display name
+				// Unwrap user
+				if let user = Auth.auth().currentUser {
+
+					// Get reference to change request
+					let changeRequest = user.createProfileChangeRequest()
+
+					// Set display name
+					changeRequest.displayName = name
+
+					// Save profile update
+					changeRequest.commitChanges { (error) in
+
+						// Catch error and print description
+						guard error == nil else {
+							print("DEBUG_PRINT: \(error!)")
+							return
+						}
+
+						// Log success
+						print("DEBUG_PRINT: Set display name: \(user.displayName!)")
+
+						// Close current scene to TabBar scene
+						self.dismiss(animated: true, completion: nil)
+					}
+				}
+			}
+		}
 	}
-	
+
     
 
     /*
