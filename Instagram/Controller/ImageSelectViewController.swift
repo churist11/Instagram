5d2bb67a3a36b9d848f37c7c24e76c998e564d32
Shoutarou.Sakurai <naritake109@yahoo.ne.jp>
2020-12-30 23:02:10 +0900

    Update SelectImage VC: Place UI (3 buttons) to the view, then connect to code

--- Instagram/Controller/ImageSelectViewController.swift
+++ Instagram/Controller/ImageSelectViewController.swift
@@ -10,14 +10,25 @@ import UIKit
 
 final class ImageSelectViewController: UIViewController {
 
+	// MARK: LifeCycle -
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
+	// MARK: IBAction -
+
+	@IBAction func handleLibraryButton(_ sender: UIButton) {
+	}
+
+	@IBAction func handleCameraButton(_ sender: UIButton) {
+	}
+
+	@IBAction func handleCancelButton(_ sender: UIButton) {
+	}
+	/*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
