7329c1e4dd31348fed5ca1e6b1d792ae209203ef
Shoutarou.Sakurai <naritake109@yahoo.ne.jp>
2020-12-30 17:48:41 +0900

    Update in setting VC : Place UI components , Add reflection display name of current user

--- Instagram/Controller/SettingViewController.swift
+++ Instagram/Controller/SettingViewController.swift
@@ -7,17 +7,46 @@
 //
 
 import UIKit
+import Firebase
 
 final class SettingViewController: UIViewController {
+
+
+	// MARK: IBOutlet -
+
+	@IBOutlet weak var textField: UITextField!
+
+
+	// MARK: Lifecycle -
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+
+		// <Reflect displayname in text field>
+		// 1-2. Get ref to the user
+		guard let user = Auth.auth().currentUser else {
+			return
+		}
+
+		// 2-2. Set display name into text field
+		self.textField.text = user.displayName
+
+	}
+
+	// MARK: IBAction -
+
+	@IBAction func handleChangeNameButton(_ sender: UIButton) {
+	}
+
+	@IBAction func handleLogOutButtton(_ sender: UIButton) {
+	}
+	/*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
