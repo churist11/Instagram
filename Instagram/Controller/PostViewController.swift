424ffbcbcc29d8edacb9c58bb0b797a8c96147ec
Shoutarou.Sakurai <naritake109@yahoo.ne.jp>
2020-12-31 16:12:44 +0900

    Update PostVC: Place UI component, and connect to code

--- Instagram/Controller/PostViewController.swift
+++ Instagram/Controller/PostViewController.swift
@@ -10,14 +10,27 @@ import UIKit
 
 final class PostViewController: UIViewController {
 
+	// MARK: - IBOutlet
+	
+	@IBOutlet weak var imageView: UIImageView!
+	@IBOutlet weak var captionTextField: UITextField!
+
+	// MARK: - LifeCycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
+	// MARK: - IBAction
+
+	@IBAction func handlePostButton(_ sender: UIButton) {
+	}
+
+	@IBAction func handleCancelButton(_ sender: UIButton) {
+	}
+	/*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
