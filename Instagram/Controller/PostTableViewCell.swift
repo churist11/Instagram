9b8cba4a480c1fdff7539a586854c077fc698e47
Shoutarou.Sakurai <naritake109@yahoo.ne.jp>
2021-01-03 13:53:06 +0900

    Create PostTableViewCell file and corresponding xib file

---
+++ Instagram/Controller/PostTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostTableViewCell.swift
+//  Instagram
+//
+//  Created by 櫻井将太郎 on 2021/01/03.
+//  Copyright © 2021 shoutarou.sakurai. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
